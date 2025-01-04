ec48af77064b598815d83fccd9743107000fa131
BOLUWATIFE-DAVID-ADANLA <adanlab4@gmail.com>
2025-01-04 14:38:07 +0100

    added navigator helper provider

--- lib/routes/navigator_helper.dart
+++ lib/routes/navigator_helper.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final navigatorhelperProvider = Provider<NavigatorHelper>((ref) {
+  return NavigatorHelper();
+});
 
 class NavigatorHelper {
   GlobalKey<ScaffoldMessengerState> scafflodMessangerKey =
