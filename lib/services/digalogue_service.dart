78ded71a2bf27701dad0033433b38d19b239c650
BOLUWATIFE-DAVID-ADANLA <adanlab4@gmail.com>
2025-01-18 23:37:24 +0100

    added error aert dialouge

--- lib/services/digalogue_service.dart
+++ lib/services/digalogue_service.dart
@@ -44,4 +44,37 @@ class DialogService {
       },
     );
   }
+
+  static Future<void> onErrorAlertDialouge({
+    required String title,
+    required String message,
+    required String svgAsset,
+  }) async {
+    final context = NavigatorHelper().navigatorKey.currentContext!;
+    await showDialog(
+      context: context,
+      builder: (_) {
+        return CustomAlertDialouge(
+          children: [
+            const AppSpacing(v: 20),
+            SvgAssets(svg: svgAsset),
+            const AppSpacing(v: 20),
+            AppTextBold(
+              text: title,
+              fontSize: 24,
+              color: AppColors.primary,
+              textAlign: TextAlign.center,
+            ),
+            const AppSpacing(v: 20),
+            AppTextRegular(
+              text: message,
+              fontSize: 16,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.clip,
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
